9e112ea985c9660449eb9861ee88589e261dde28
Kevin Smith <keveightysev@gmail.com>
2019-01-22 16:46:07 -0600

    Stretch: completed challenge 3 on closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -37,4 +37,26 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  let obj = {
+    increment: function(){
+      return count += 1;
+    }, 
+    decrement: function(){
+      return count -= 1;
+    }
+  };
+  return obj;
 };
+
+let thisCounter = counterFactory();
+
+console.log(thisCounter.increment());
+console.log(thisCounter.increment());
+console.log(thisCounter.increment());
+console.log(thisCounter.decrement());
+console.log(thisCounter.decrement());
+console.log(thisCounter.decrement());
+console.log(thisCounter.decrement());
+console.log(thisCounter.decrement());
+console.log(thisCounter.decrement());
