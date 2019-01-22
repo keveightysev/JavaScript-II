b8a9c02f1fa91318b9efee9275cc7a77d8375799
Kevin Smith <keveightysev@gmail.com>
2019-01-22 15:16:21 -0600

    Completed Task 4 by finishing non-stretch challenges ins closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function greeting() {
+  let name = "Kevin";
+  console.log(`Hello there, ${name}!`)
+}
+
+let name = "Richard";
+
+greeting();
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return count++;
+  };
 };
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
